b54cc74cdc209dd7d76f4437ba2cdc8f260bd2ea
minjunKim <dsilencehell@gmail.com>
2024-07-09 16:13:48 +0900

    fix  - 이름 변경

--- src/main/java/dz/shared/util/queryDsl/QueryDslUtils.java
+++ src/main/java/dz/shared/util/queryDsl/QueryDslUtils.java
@@ -11,7 +11,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
-public class QuerydslUtils {
+public class QueryDslUtils {
     //
     public static BooleanExpression eqIfNotNull(StringPath target, String value) {
         //
